3d390a6ba5ac86b23ed196be8ccf265c2bd4aafa
mjrajput04 <manojvagadiya04@gmail.com>
2026-05-01 12:37:52 +0530

    change in ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,6 +19,23 @@ module.exports = {
       merge_logs: true,
       log_type: 'json',
       time: true
+    },
+    {
+      name: 'upload-server',
+      script: 'upload-server.js',
+      cwd: '/var/www/millerstorm',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '2G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 6788
+      },
+      error_file: '/var/www/millerstorm/logs/upload-err.log',
+      out_file: '/var/www/millerstorm/logs/upload-out.log',
+      merge_logs: true,
+      time: true
     }
   ]
 };
